533e3091da64e02aced9d4cc8a96116ba6894188
tuhinx <20234208032@eee.bubt.edu.bd>
2026-01-05 09:55:57 +0600

    Bug Fixed.

--- android-pdf-viewer/src/main/java/com/github/pdfviewer/listener/OnLoadCompleteListener.java
+++
@@ -1,9 +0,0 @@
-
-package com.github.pdfviewer.listener;
-
-
-public interface OnLoadCompleteListener {
-
-    
-    void loadComplete(int nbPages);
-}
--- android-pdf-viewer/src/main/java/com/github/pdfviewer/util/FitPolicy.java
+++
@@ -1,6 +0,0 @@
-
-package com.github.pdfviewer.util;
-
-public enum FitPolicy {
-    WIDTH, HEIGHT, BOTH
-}
