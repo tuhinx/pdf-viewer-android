5144d1a367ea5a0011ab05228c27a8ddbed0992f
tuhinx <20234208032@eee.bubt.edu.bd>
2026-01-05 10:05:23 +0600

    Bug Fixed.

--- android-pdf-viewer/src/main/java/com/github/pdfviewer/listener/OnPageErrorListener.java
+++
@@ -1,8 +0,0 @@
-
-package com.github.pdfviewer.listener;
-
-public interface OnPageErrorListener {
-
-    
-    void onPageError(int page, Throwable t);
-}
--- android-pdf-viewer/src/main/java/com/github/pdfviewer/util/SnapEdge.java
+++
@@ -1,6 +0,0 @@
-
-package com.github.pdfviewer.util;
-
-public enum SnapEdge {
-    START, CENTER, END, NONE
-}
